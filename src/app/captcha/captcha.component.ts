00eb9939262386b25a9448768cfecd14f232db8e
kurizma <foriamkuri@gmail.com>
2025-11-05 19:31:42 +0200

    workspace added, components made

---
+++ src/app/captcha/captcha.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-captcha',
+  imports: [],
+  templateUrl: './captcha.component.html',
+  styleUrl: './captcha.component.css'
+})
+export class CaptchaComponent {
+
+}
